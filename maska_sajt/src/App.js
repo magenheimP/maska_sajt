93a99e3c946d88bb634e1066c4bf7fcd9c7409de
DimitrijeInfaloom <dimitrije@infaloom.com>
2024-10-30 19:35:36 +0100

    Scrollers

--- maska_sajt/src/App.js
+++ maska_sajt/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import VerticalScroller from './VerticalScroller';
+import HorizontalScroller from './HorizontalScroller';
 
 function App() {
   return (
     <div className="App">
-
+      {/* <div className="block md:hidden">
+        <VerticalScroller />
+      </div> */}
+      {/* <div className="hidden md:block">
+        <HorizontalScroller />
+      </div> */}
     </div>
   );
 }
---
+++ maska_sajt/src/HorizontalScroller.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+
+const images = [
+    // "./horizontal/_DSC1202.jpg",
+    "./horizontal/DSC_4127.jpg"
+];
+
+const HorizontalScroller = () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+        setIsVisible(true);
+      }, 1500);
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIndex) =>
+        prevIndex === images.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="relative w-full h-screen overflow-hidden flex items-center justify-center">
+        <img src="./logo/beli.png" alt="logo" className={`w-[40%] z-20 transition-opacity duration-1000 ${isVisible ? 'opacity-100' : 'opacity-0'}`}/>
+        <div className="absolute inset-0 bg-black opacity-65 z-10"></div>
+      {images.map((image, index) => (
+        <img
+          key={index}
+          src={image}
+          alt={`Slika ${index + 1}`}
+          className={`absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-1000 ${
+            index === currentImageIndex ? "opacity-100" : "opacity-0"
+          }`}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default HorizontalScroller;
